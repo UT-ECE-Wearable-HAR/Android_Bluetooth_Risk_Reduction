e0345867d4a7f284141e8c2d08a02db86d23e96c
Aman Hemani <aman546052@gmail.com>
2020-11-13 00:50:12 -0600

    prompt user to turn on bluetooth

--- app/src/main/java/com/example/bluetoothrr/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothrr/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.bluetoothrr
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.system.exitProcess
+
+const val REQUEST_ENABLE_BT = 5
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null){
+            exitProcess(-1)
+        }
+        if(!bluetoothAdapter.isEnabled){
+            val enableBTIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT)
+        }
+
     }
 }
